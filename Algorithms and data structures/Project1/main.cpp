1eae40ea6e5502b640c5074c72c9da7d8136a137
S1LenTFeaR <killerus34@gmail.com>
2020-03-09 11:13:02 +0300

    Lab2CMat

---
+++ Algorithms and data structures/Project1/main.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+#include <iomanip>
+#include <fstream>
+#include <random> 
+#include <ctime>  
+
+using namespace std;
+
+int SelectAction()
+{
+	int action;
+	cout << "Меню:" << endl;
+	cout << "1 / пузырик" << endl;
+	cout << "2 / быстрая" << endl;
+	cout << "3 / турнирная" << endl;
+	cout << "4 / завершить" << endl;
+	cin >> action;
+	return action;
+}
+
+template<class T>
+T Puzyrik(T* arr, int size)
+{
+	double temp;
+
+	// Сортировка массива пузырьком
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = 0; j < size - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				// меняем элементы местами
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				for (int j = 0; j < size; j++)
+				{
+					cout << arr[j] << " ";
+				}
+				cout << endl;
+			}
+		}
+	}
+	return *arr;
+}
+
+template<class T>
+T quickSort(T* arr, int left, int right)
+{
+	T pivot; // разрешающий элемент
+	int l_hold = left; //левая граница
+	int r_hold = right; // правая граница
+	int n = right;
+	pivot = arr[left];
+	while (left < right) // пока границы не сомкнутся
+	{
+		while ((arr[right] >= pivot) && (left < right))
+			right--; // сдвигаем правую границу пока элемент [right] больше [pivot]
+		if (left != right) // если границы не сомкнулись
+		{
+			arr[left] = arr[right]; // перемещаем элемент [right] на место разрешающего
+			left++; // сдвигаем левую границу вправо
+			for (int j = 0; j < n; j++)
+			{
+				cout << arr[j] << " ";
+			}
+			cout << endl;
+		}
+		while ((arr[left] <= pivot) && (left < right))
+			left++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
+		if (left != right) // если границы не сомкнулись
+		{
+			arr[right] = arr[left]; // перемещаем элемент [left] на место [right]
+			right--; // сдвигаем правую границу вправо
+			for (int j = 0; j < n; j++)
+			{
+				cout << arr[j] << " ";
+			}
+			cout << endl;
+		}
+	}
+	arr[left] = pivot; // ставим разрешающий элемент на место
+	for (int j = 0; j < n; j++)
+	{
+		cout << arr[j] << " ";
+	}
+	cout << endl;
+	pivot = left;
+	left = l_hold;
+	right = r_hold;
+	if (left < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
+		quickSort(arr, left, pivot - 1);
+	if (right > pivot)
+		quickSort(arr, pivot + 1, right);
+	return *arr;
+}
+
+template<class T>
+void heapify(T* arr, int n, int i)
+{
+	int largest = i;
+	// Инициализируем наибольший элемент как корень
+	int l = 2 * i + 1; // левый = 2*i + 1
+	int r = 2 * i + 2; // правый = 2*i + 2
+
+ // Если левый дочерний элемент больше корня
+	if (l < n && arr[l] > arr[largest])
+		largest = l;
+
+	// Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+	if (r < n && arr[r] > arr[largest])
+		largest = r;
+
+	// Если самый большой элемент не корень
+	if (largest != i)
+	{
+		swap(arr[i], arr[largest]);
+
+		// Рекурсивно преобразуем в двоичную кучу затронутое поддерево
+		heapify(arr, n, largest);
+	}
+}
+
+// Основная функция, выполняющая пирамидальную сортировку
+template<class T>
+void heapSort(T* arr, int n)
+{
+	// Построение кучи (перегруппируем массив)
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	// Один за другим извлекаем элементы из кучи
+	for (int i = n - 1; i >= 0; i--)
+	{
+		// Перемещаем текущий корень в конец
+		swap(arr[0], arr[i]);
+
+		// вызываем процедуру heapify на уменьшенной куче
+		heapify(arr, i, 0);
+	}
+}
+
+template <class T>
+void intgen(int n)
+{
+	T* arr;
+	T* arr1;
+	arr = new T[n];
+	arr1 = new T[n];
+	default_random_engine gen(time(NULL));
+	uniform_int_distribution<T> dis(1, 50);
+	for (int i = 0; i < n; i++)
+	{
+		arr1[i] = dis(gen);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr1[i] << " ";
+	}
+	cout << endl;
+	int key;
+	do
+	{
+		key = SelectAction();
+		switch (key)
+		{
+		case 1:
+			for (int i = 0; i < n; i++)
+			{
+				arr[i] = arr1[i];
+			}
+			cout << "Сортировка началась" << endl;
+			Puzyrik(arr, n); //Сортировка пузырьком
+			cout << "Сортировка закончилась" << endl;
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << " ";
+			}
+			cout << endl;
+			break;
+		case 2:
+			for (int i = 0; i < n; i++)
+			{
+				arr[i] = arr1[i];
+			}
+			cout << "Сортировка началась" << endl;
+			quickSort(arr, 0, n - 1); //Быстрая сортировка
+			cout << "Сортировка закончилась" << endl;
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << " ";
+			}
+			cout << endl;
+			break;
+		case 3:
+			for (int i = 0; i < n; i++)
+			{
+				arr[i] = arr1[i];
+			}
+			cout << "Сортировка началась" << endl;
+			heapSort(arr, n);
+			cout << "Сортировка закончилась" << endl;
+			for (int i = 0; i < n; i++)
+			{
+				cout << arr[i] << " ";
+			}
+			cout << endl;
+			break;
+		default:
+			key = -1;
+			break;
+		}
+	} while (key != -1);
+	delete[] arr;
+	delete[] arr1;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	intgen<int>(10);
+
+
+	return 0;
+}
--- Computational Mathematics/Computational Mathematics.cpp
+++ Computational Mathematics/Computational Mathematics.cpp
@@ -1,253 +1,68 @@
 ﻿#include <iostream>
 #include <cmath>
 #include <clocale>
+#include <iomanip>
 #include <fstream>
 using namespace std;
 
-double f1(double x)
+double f(double x)
 {
-	return sqrt(x + 1) - 1 / x;
+    return x * sin(x);
 }
 
-double proizvod(double x, double h, double f(double x))
+double fu(double x)
 {
-	return (f(x + h) - f(x)) / h;
+    return x * sqrt(x);
 }
 
-double f2(double x)
+double Rectangle(double x, double xn, double h) //По методу прямоугольника
 {
-	double pi = 3.1415926536;
-	double alpha = x / 180 * pi;
-	return pow(x, 3) - 7 * pow(sin(alpha), 3) + x - 4;
+    double S = 0;
+    for (x; x < xn; x += h)
+    {
+        S += f(x);
+    }
+    return S *= h;
 }
 
-double Nuton(double eps, double Xn, double h, double f(double Xn))
+double Trapeze(double x, double xn, double h) //По методу трапеции
 {
-	cout << "f(" << Xn << ") = " << f(Xn) << endl;
-	for (int i = 0; eps <= abs(f(Xn)); i++)
-	{
-		Xn = Xn - f(Xn) / proizvod(Xn, h, f);
-		cout << "f(" << Xn << ") = " << f(Xn) << endl;
-	}
-	cout << endl;
-	return Xn;
+    double S = 0.5 * (f(x) + f(xn));
+    for (x += h; x < xn; x += h)
+    {
+        S += f(x);
+    }
+    return S *= h;
 }
 
-double diht(double eps, double x0, double x1, double f(double x))
+double Simpson(double x, double xn, double h, int N) //По методу Симпсона
 {
-
-	double x2;
-	while (x1 - x0 > eps) 
-	{
-		x2 = (x0 + x1) / 2;
-		if (f(x2) * f(x1) > 0)
-		{
-			x1 = x2;
-			cout << "f(" << x2 << ") = " << f(x2) << endl;
-		}
-		else
-		{
-			x0 = x2;
-			cout << "f(" << x2 << ") = " << f(x2) << endl;
-		}
-	}
-	return 0.5 * (x0 + x1);
+    double I, I2 = 0, I4 = 0;
+    I4 = f(x + h);
+    for (int i = 2; i < N; i += 2)
+    {
+        if (N > 3)
+            I4 += f(x + (i + 1) * h);
+        I2 += f(x + i * h);
+    }
+    I = f(x) + f(xn) + 4 * I4 + 2 * I2;
+    I *= h / 3;
+    return I;
 }
 
-double func(double x)
+void main()
 {
-	return (pow(x, 6) + pow(x, 3) - 2) / sqrt(1 - pow(x, 3));
+    setlocale(LC_ALL, "Russian");
+    double pi = 3.14159265;
+    double a = 0, b = 2 * pi;
+    int N = 4;
+    double h = (b - a) / N;
+    cout << "N по методу прямоугольника: " << Rectangle(a, b, h) << endl;
+    cout << "N по методу трапеции: " << Trapeze(a, b, h) << endl;
+    cout << "N по методу симпсона: "  << Simpson(a, b, h, N) << endl;
+    N *= 2;
+    h = (b - a) / N;
+    cout << "2N по методу прямоугольника: " << Rectangle(a, b, h) << endl;
+    cout << "2N по методу трапеции: " << Trapeze(a, b, h) << endl;
+    cout << "2N по методу симпсона: " << Simpson(a, b, h, N) << endl;
 }
-
-double proizvodX2(double x, double h, double f(double x))
-{
-	return (f(x + h) - f(x - h)) / (2 * h);
-}
-
-double proizvod2(double x, double h, double f(double x))
-{
-	return (f(x + h) - 2 * f(x) + f(x - h)) / (h * h);
-}
-
-double proizvod3(double x, double h, double f(double x))
-{
-	return (f(x + 2 * h) - 2 * f(x + h) + 2 * f(x - h) - f(x - 2 * h)) / (2 * pow(h, 3));
-}
-
-void func(double a, double h, double b, double N, ofstream& file, double f(double x))
-{
-	double c = b - a;
-	cout << N << "N" << endl;
-	file << N << "N" << endl;
-	c /= N;
-	if(c == 0)
-	{ 
-		cout << "f(" << a << ") =" << f(a) << endl;
-		file << a << ";" << f(a) << endl;
-	}
-	else
-	{ 
-		for (a; a <= b; a += c)
-		{
-			cout << "f(" << a << ") =" << f(a) << endl;
-			file << a << ";" << f(a) << endl;
-		}
-	}
-	cout << endl;
-}
-
-void func(double a, double h, double b, double N, ofstream& file, double f(double x, double h, double f(double x)))
-{
-	double c = b - a;
-	cout << N << "N" << endl;
-	file << N << "N" << endl;
-	c /= N;
-	if (c == 0)
-	{
-		cout << "f(" << a << ") =" << f(a, h, func) << endl;
-		file << a << ";" << f(a, h, func) << endl;
-	}
-	else
-	{ 
-		for (a; a <= b; a += c)
-		{
-			cout << "f(" << a << ") =" << f(a, h, func) << endl;
-			file << a << ";" << f(a, h, func) << endl;
-		}
-	}
-	cout << endl;
-}
-
-int SelectAction()
-{
-	int action;
-	cout << "Меню:" << endl;
-	cout << "1 / найти x, где f(x) = 0 в sqrt(x+1)-1/x методом Ньютона" << endl;
-	cout << "2 / найти x, где f(x) = 0 в x^3-7*sin^3(x)+x-4/x методом Ньютона" << endl;
-	cout << "3 / найти x, где f(x) = 0 в x^3-7*sin^3(x)+x-4/x методом дихтонии" << endl;
-	cout << "4 / вычислить функцию (x^6+x^3-2)/sqrt(1-x^3)" << endl;
-	cout << "5 / вычислить первую производную функции (x^6+x^3-2)/sqrt(1-x^3)" << endl;
-	cout << "6 / вычислить первую производную функции с x2 точностью (x^6+x^3-2)/sqrt(1-x^3)" << endl;
-	cout << "7 / вычислить вторую производную функции (x^6+x^3-2)/sqrt(1-x^3)" << endl;
-	cout << "8 / вычислить третью производную функции (x^6+x^3-2)/sqrt(1-x^3)" << endl;
-	cout << "9 / вычислить первую производную функции (x^6+x^3-2)/sqrt(1-x^3) аналит" << endl;
-	cout << "10 / вычислить вторую производную функции (x^6+x^3-2)/sqrt(1-x^3) аналит" << endl;
-	cout << "11 / вычислить третью производную функции (x^6+x^3-2)/sqrt(1-x^3) аналит" << endl;
-	cout << "12 / завершить" << endl;
-	cin >> action;
-	return action;
-}
-
-double proizvoda1(double x)
-{
-	return (3 * pow(x, 2) + 6 * pow(x, 5)) / sqrt(1 - pow(x, 3)) + 3 * pow(x, 2) * (pow(x, 6) + pow(x, 3) - 2) / (2 * pow((1 - pow(x, 3)), 1.5));
-}
-
-double proizvoda2(double x)
-{
-	return 3 * x * (2 + 10 * pow(x, 3) + 3 * pow(x, 3) * (1 + 2 * pow(x, 3)) / (1 - pow(x, 3)) - (-4 + 9 * pow(x, 3) / (-1 + pow(x, 3))) * (-2 + pow(x, 3) + pow(x, 6)) / (4 * (1 - pow(x, 3)))) / sqrt(1 - pow(x, 3));
-}
-
-double proizvoda3(double x)
-{
-	return 3 * (2 + 40 * pow(x, 3) + 9 * pow(x, 3) * (1 + 5 * pow(x, 3)) / (1 - pow(x, 3)) + (-2 + pow(x, 3) + pow(x, 6)) * (8 - 108 * pow(x, 3) / (-1 + pow(x, 3)) + 135 * pow(x, 6) / pow((-1 + pow(x, 3)), 2)) / (8 * (1 - pow(x, 3))) - 9 * pow(x, 3) * (1 + 2 * pow(x, 3)) * (-4 + 9 * pow(x, 3) / (-1 + pow(x, 3))) / (4 * (1 - pow(x, 3)))) / sqrt(1 - pow(x, 3));
-}
-
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	ofstream file("f(x).txt");
-	double h = 0.01, eps = 0.5 * pow(10, -3);
-	double a, b, N;
-	cout << "Введите начало отрезка a" << endl;
-	cin >> a;
-	cout << "Введите конец отрезка b" << endl;
-	cin >> b;
-	int key;
-	do 
-	{	
-		key = SelectAction();
-		switch (key)
-		{
-		case 1:
-			cout << "x = " << Nuton(eps, a, h, f1) << endl;
-			break;
-		case 2:
-			cout << "x = " << Nuton(eps, a, h, f2) << endl;
-			break;
-		case 3:
-			cout << "x = " << diht(eps, a, b, f2) << endl;
-			break;
-		case 4:
-			N = 1;
-			func(a, h, b, N, file, func);
-			N = 2;
-			func(a, h, b, N, file, func);
-			N = 4;
-			func(a, h, b, N, file, func);
-			break;
-		case 5:
-			N = 1;
-			func(a, h, b, N, file, proizvod);
-			N = 2;
-			func(a, h, b, N, file, proizvod);
-			N = 4;
-			func(a, h, b, N, file, proizvod);
-			break;
-		case 6:
-			N = 1;
-			func(a, h, b, N, file, proizvodX2);
-			N = 2;
-			func(a, h, b, N, file, proizvodX2);
-			N = 4;
-			func(a, h, b, N, file, proizvodX2);
-			break;
-		case 7:
-			N = 1;
-			func(a, h, b, N, file, proizvod2);
-			N = 2;
-			func(a, h, b, N, file, proizvod2);
-			N = 4;
-			func(a, h, b, N, file, proizvod2);
-			break;
-		case 8:
-			N = 1;
-			func(a, h, b, N, file, proizvod3);
-			N = 2;
-			func(a, h, b, N, file, proizvod3);
-			N = 4;
-			func(a, h, b, N, file, proizvod3);
-			break;
-		case 9:
-			N = 1;
-			func(a, h, b, N, file, proizvoda1);
-			N = 2;
-			func(a, h, b, N, file, proizvoda1);
-			N = 4;
-			func(a, h, b, N, file, proizvoda1);
-			break;
-		case 10:
-			N = 1;
-			func(a, h, b, N, file, proizvoda2);
-			N = 2;
-			func(a, h, b, N, file, proizvoda2);
-			N = 4;
-			func(a, h, b, N, file, proizvoda2);
-			break;
-		case 11:
-			N = 1;
-			func(a, h, b, N, file, proizvoda3);
-			N = 2;
-			func(a, h, b, N, file, proizvoda3);
-			N = 4;
-			func(a, h, b, N, file, proizvoda3);
-			break;
-		default:
-			key = -1;
-			break;
-		}
-	} while (key != -1);
-	file.close();
-	return 0;
-}
-
